d9a56908db21dbb51ddb8daf410c061bb94da060
leandroszikora <31076665+leandroszikora@users.noreply.github.com>
2022-04-28 20:21:30 -0300

    Clase 1 - Ejemplo KDA

---
+++ clase-1/S3Sink/src/main/java/com/amazonaws/services/kinesisanalytics/S3StreamingSinkJob.java
@@ -0,0 +1,67 @@
+package com.amazonaws.services.kinesisanalytics;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.JsonNode;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
+import org.apache.flink.streaming.connectors.kinesis.FlinkKinesisConsumer;
+import org.apache.flink.streaming.connectors.kinesis.config.ConsumerConfigConstants;
+import org.apache.flink.api.common.serialization.SimpleStringEncoder;
+import org.apache.flink.core.fs.Path;
+import org.apache.flink.streaming.api.functions.sink.filesystem.StreamingFileSink;
+import org.apache.flink.util.Collector;
+
+import java.util.Properties;
+
+public class S3StreamingSinkJob {
+    private static final String region = "us-east-1";
+    private static final String inputStreamName = "ExampleInputStream";
+    private static final String s3SinkPath = "s3a://<YOUR_BUCKET>/data";
+
+    private static DataStream<String> createSourceFromStaticConfig(StreamExecutionEnvironment env) {
+
+        Properties inputProperties = new Properties();
+        inputProperties.setProperty(ConsumerConfigConstants.AWS_REGION, region);
+        inputProperties.setProperty(ConsumerConfigConstants.STREAM_INITIAL_POSITION,
+                "LATEST");
+        return env.addSource(new FlinkKinesisConsumer<>(inputStreamName,
+                new SimpleStringSchema(),
+                inputProperties));
+    }
+
+    private static StreamingFileSink<String> createS3SinkFromStaticConfig() {
+
+        final StreamingFileSink<String> sink = StreamingFileSink
+                .forRowFormat(new Path(s3SinkPath), new SimpleStringEncoder<String>("UTF-8"))
+                .build();
+        return sink;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        DataStream<String> input = createSourceFromStaticConfig(env);
+
+        ObjectMapper jsonParser = new ObjectMapper();
+
+        input.map(value -> { // Parse the JSON
+            JsonNode jsonNode = jsonParser.readValue(value, JsonNode.class);
+            return new Tuple2<>(jsonNode.get("event_name").toString(), 1); // hover, 1 - popup, 1
+        }).returns(Types.TUPLE(Types.STRING, Types.INT))
+                .keyBy(0) // Logically partition the stream for each word
+                // .timeWindow(Time.minutes(1)) // Tumbling window definition // Flink 1.11
+                .window(TumblingProcessingTimeWindows.of(Time.minutes(1))) // Flink 1.13
+                .sum(1) // Count the appearances by ticker per partition
+                .map(value -> value.f0 + " count: " + value.f1.toString() + "\n")
+                .addSink(createS3SinkFromStaticConfig());
+
+        env.execute("Flink S3 Streaming Sink Job");
+    }
+}
